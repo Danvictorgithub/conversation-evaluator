40d9935c9dddbe3ccdc48a1b67653ed838c18a6b
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-04-16 17:31:29 +0800

    created main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+# The main.py is responsible for automating the conversation evaluation
+import requests
+from dotenv import load_dotenv
+import json
+import os
+from db import Evaluation, Session
+
+load_dotenv()
+
+
+def evaluate_conversation(text):
+    api = f"{os.getenv('KILN_PORT')}/api/projects/{os.getenv('PROJECT_ID')}/tasks/{os.getenv('TASK_ID')}/run"
+    print("API URL:", api)
+    response = requests.post(
+        api,
+        json={
+            "model_name": "gpt_4o_mini",
+            "provider": "openrouter",
+            "plaintext_input": text,
+            "ui_prompt_method": os.getenv("PROMPT_ID"),
+        },
+    )
+    if not response.ok:
+        return {
+            "error": "Failed to evaluate conversation",
+            "status_code": response.status_code,
+        }
+    return response.json()
+
+
+def get_output(response):
+    return response["output"]["output"]
+
+
+def json_preprocess(response_text):
+    response_text = get_output(response_text)
+    # Extract the JSON part from the text
+    json_start = response_text.find("{")
+    json_end = response_text.rfind("}") + 1
+    json_data = response_text[json_start:json_end]
+
+    # Parse the JSON string into a Python dictionary
+    try:
+        parsed_json = json.loads(json_data)
+        return parsed_json
+    except json.JSONDecodeError as e:
+        print("Error decoding JSON:", e)
+        return None
+
+
+def save_to_db(json_data, model_name):
+    with Session() as session:
+        evaluation = Evaluation(
+            model_name=model_name,
+            grammatical_correctness=json_data["grammatical_correctness"],
+            readability=json_data["readability"],
+            descriptiveness=json_data["descriptiveness"],
+            coherence=json_data["coherence"],
+            conciseness=json_data["conciseness"],
+            explanation=json_data["explanation"],
+        )
+        session.add(evaluation)
+        session.commit()
+
+
+def main():
+    # response = evaluate_conversation(
+    #     "Person A: Hi.  Person B: Hello.  Person A: How are you.  Person B: Yes.  Person A: Ok.  Person B: Bye."
+    # )
+    # sample_model = "sliding-sparse"
+    # json_data = json_preprocess(response)
+    # if json_data:
+    #     save_to_db(json_data, sample_model)
+    #     print("Data saved to database successfully.")
+    # else:
+    #     print("Failed to process JSON data.")
+    # print(json_preprocess(response))
+
+
+if __name__ == "__main__":
+    main()
