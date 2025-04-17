3fdbf81abba8b4859a06deff44c3ee5b511e6471
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-04-17 10:11:26 +0800

    created provider

--- provider.py
+++ provider.py
@@ -1,5 +1,26 @@
-import requests
+import subprocess
+import random
+import os
+from dotenv import load_dotenv
 
-def generate_conversation():
+load_dotenv()
 
 
+def generate_conversation_full():
+    seed = random.randint(0, 1000000)
+    llm_folder = os.getenv("LLM_FOLDER", "")
+    command = f"python sample_gen.py --out_dir=out-full-wfull-54M-r5 --seed={seed}"
+
+    try:
+        result = subprocess.run(
+            command,
+            shell=True,
+            check=True,
+            text=True,
+            capture_output=True,
+            cwd=llm_folder,
+        )
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e.stderr}")
+        return None
